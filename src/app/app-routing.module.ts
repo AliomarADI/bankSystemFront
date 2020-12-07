594a77b3cef4ed7a8b2a575f78cae4ab8e4359f6
Echo <aliomaramerica@gmail.com>
2020-12-08 00:34:30 +0600

    2 day

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainpageComponent} from './components/mainpage/mainpage.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {TransactionComponent} from './components/transaction/transaction.component';
+import {TransferComponent} from './components/transfer/transfer.component';
+import {TestPageComponent} from './components/elements/test-page/test-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageComponent},
+  {path: 'main', component: MainpageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'transaction', component: TransactionComponent},
+  {path: 'transfer', component: TransferComponent},
+  {path: 'testpage', component: TestPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { CarouselComponent } from './components/elements/carousel/carousel.component';
+import { EmptySpaceComponent } from './components/elements/empty-space/empty-space.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { TransferBlockComponent } from './components/elements/transfer-block/transfer-block.component';
+import { TransactionBlockComponent } from './components/elements/transaction-block/transaction-block.component';
+import { ProfileBlockComponent } from './components/elements/profile-block/profile-block.component';
+import { TestPageComponent } from './components/elements/test-page/test-page.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent,
+    CarouselComponent,
+    EmptySpaceComponent,
+    LoginComponent,
+    RegisterComponent,
+    TransferComponent,
+    TransactionComponent,
+    TransferBlockComponent,
+    TransactionBlockComponent,
+    ProfileBlockComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
